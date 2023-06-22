a1bd2b7b8fdbb68029729210be1fbb01413c981c
0xtito <welovethepenguins@gmail.com>
2023-06-22 13:00:34 -0400

    final scripts

--- scripts/createSigner.ts
+++ scripts/createSigner.ts
@@ -3,14 +3,13 @@ import {
   SimpleSmartContractAccount,
   SmartAccountProvider,
 } from "@alchemy/aa-core";
-import { privateKeyToAccount, mnemonicToAccount } from "viem/accounts";
+import { privateKeyToAccount } from "viem/accounts";
 import { toHex } from "viem/utils";
 import { sepolia } from "viem/chains";
 import * as dotenv from "dotenv";
 dotenv.config();
 
 const PRIV_KEY = process.env.PRIV_KEY!;
-const MNEMONIC_PHRASE = process.env.MNEMONIC_PHRASE!;
 const ALCHEMY_API_URL = process.env.ALCHEMY_API_URL!;
 
 const ENTRYPOINT_ADDRESS = "0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789";
@@ -18,10 +17,7 @@ const SIMPLE_ACCOUNT_FACTORY_ADDRESS =
   "0x9406Cc6185a346906296840746125a0E44976454";
 
 export default async function createSigner() {
-  const account =
-    PRIV_KEY !== ""
-      ? privateKeyToAccount(`0x${PRIV_KEY}`)
-      : mnemonicToAccount(MNEMONIC_PHRASE);
+  const account = privateKeyToAccount(`0x${PRIV_KEY}`);
 
   const owner: SimpleSmartAccountOwner = {
     signMessage: async (msg) =>
--- scripts/sendEthToSCA.ts
+++ scripts/sendEthToSCA.ts
@@ -1,22 +1,17 @@
 import { parseEther } from "viem";
-import { privateKeyToAccount, mnemonicToAccount } from "viem/accounts";
-import { createWalletClient, custom, http } from "viem";
+import { privateKeyToAccount } from "viem/accounts";
+import { createWalletClient, http } from "viem";
 import { sepolia } from "viem/chains";
-import { toHex } from "viem/utils";
 import * as dotenv from "dotenv";
 dotenv.config();
 
 import { counterfactualAddress } from "../accountInfo.json";
 
 const PRIV_KEY = process.env.PRIV_KEY!;
-const MNEMONIC_PHRASE = process.env.MNEMONIC_PHRASE!;
 const ALCHEMY_API_URL = process.env.ALCHEMY_API_URL!;
 
 async function main() {
-  const account =
-    PRIV_KEY !== ""
-      ? privateKeyToAccount(`0x${PRIV_KEY}`)
-      : mnemonicToAccount(MNEMONIC_PHRASE);
+  const account = privateKeyToAccount(`0x${PRIV_KEY}`);
 
   const wallet = createWalletClient({
     account: account,
@@ -26,7 +21,7 @@ async function main() {
 
   const txHash = await wallet.sendTransaction({
     to: counterfactualAddress as `0x${string}`,
-    value: parseEther("0.01"),
+    value: parseEther("0.5"),
   });
 
   return txHash;
